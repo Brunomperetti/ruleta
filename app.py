ddf2c71525de661a177b5c1af22aebc4cf0c6b81
Bruno Peretti <90000878+Brunomperetti@users.noreply.github.com>
2025-07-04 13:49:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,33 +2,14 @@ import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
 import urllib.parse
-from datetime import datetime
 import gspread
-from google.oauth2.service_account import Credentials
 
 # --- GOOGLE SHEETS CONFIG ---
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 SPREADSHEET_ID = "1P8YJsjXVU3rqUH4zJx2vM0qtT43PsDfXuZ5nOhbW_sc"
 SHEET_NAME = "Sheet1"
 
-# 🔑 Credenciales embebidas directamente en el código
-SERVICE_ACCOUNT_INFO = {
-  "type": "service_account",
-  "project_id": "proyectomillexsheet",
-  "private_key_id": "1a4ae0b3a31e6741708886460a3675162d111f97",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCNY7Sug2b6mE44\nDUs9UWCluDjb4TspGAe1eyPHp+ux8kcWRuAknVzFZSg6hJsm9dL9ST3yazKGhogk\n7cQKS+AEIEbXCiyKmT+0Hlzrs+3lJp6peNrXilUgg0atG31+As0acTO0nSEzeY0F\nurPj+YVeZJqfuDQ7Kl1ZL6dLt8fFrw63N2/QptqmMU2N3/4Tf4PA/FnrxcXIyflQ\ni6gWrJZ++MbvMjJuL0u9Wu8ibHQIzeBenBklmXiejIgarY3uvhI36LtG+eFbEvz2\nOiicyRftTYPacJNxZeakz2Oqg+pqU/ry9xMUvkQs7dfZtAHXYz6cOZ4BkxoqjT2/\n97he2cxxAgMBAAECggEABoZp8b3VtE3wGi0e4ksNo0g/s4IO+WQHAwyWR6ILwhAU\n0MXflUD/5Yo4E49IG8GvhYnqRnRFBilSQiYI7JK42Wfl7QmmqZTN+FGIZ0ZL/c3M\n97RAoT7ck94LFnUU0wptDcHYqYaw4A/FftSxdZ1v1r3c1u8o1ewsF7AXz+pA2XIb\nsAMkDg6TNJlEO35RUd5qa8gm5pU/HxaKfbMwCsZP5px0lnG3M0el2SkSPlspq51n\nr+5AJMcOneJngZulpkaWEkA5uZjac59mgzHHt8QpPZTuRFccpNzIfXlJ0BRBFFt+\najY78jXG4EWKXtDH9K2x2P2CyNf9JqskcWKrIWbWeQKBgQDGPBCYwIxAZvUH2YQ3\nOUEqR/gr7Sl+f+0uHXlKc98QqwUAmLCi954eEVBSEVJ5uTimb48lTkQ8wBuFm8rG\ncmpxS9i3opznjqJjg0ssEv81eQ0lClyN69eWBeVJsJdfVzd1XH8StyknCQrhM0pi\nKAB5U9104xduqfs38eS3VtJ2HQKBgQC2lxkfVJxWLhkIfH2QlMZYRWDjvlrKXeyz\nJAhbhejulDqadaDzTINJSBYBiwlNETzVmsCZWzsLzBKEH7wz9gWk1FZwQLuZ/Fwa\nxid+FDsil54KJdS16wFwzina4wXLpam5Xw7R/IV9dXJAIa5Uhr6wkm8OC8EI2WCf\n5L+3AYOPZQKBgG+Hi67lZgM16dYowwJu7ALlyvKr1BSf1MmFO6Bv4Kh4D7BuN7L4\nrbUs+IuzwztIwl6hlV/f0PR9AP8Cz+smpExp59wpWS3OSf7C96Asb/mdyY91bO1M\nK1b0qjPzbrbtUv5ss/HLqxOTOtPJD6h56QpZ7Na+jYhtijHHnm6oInDhAoGABon0\nuV2DV8bA2L/0kzfwvnqFlMJdZ//jGStCLVznUZv3WU67G9tynC47s4RyKdNjNVcc\nbKGKxMpjI14rlETMQPHlwpFe5o3WEpNkGKzKzVtWqpBeRMGMxKhtcUxOCdP99wiD\ndFuxwCNzmv5ZyuckbxhfZ8Zd/F4kirVbdEdWmc0CgYAHcC7l8LZiVLnyf/jderZj\nKmO6Vcj0tg9+HYacV622pvBr9+3zx0m1TVnsK9V9vca39zMDHiBtkTMuhKB7MLT9\nFdr5kG/58hOjNNl5br6vPYUWmbLLTRQRAnBfuLAnL2fOOMdyouPPFjI2EU7MeuIn\nW8RoNRCC9kNICCEiSJ97lw==\n-----END PRIVATE KEY-----\n",
-  "client_email": "mi-app-python@proyectomillexsheet.iam.gserviceaccount.com",
-  "client_id": "116527539433049981352",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/mi-app-python%40proyectomillexsheet.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-}
-
-# Crear las credenciales desde el diccionario
-credentials = Credentials.from_service_account_info(SERVICE_ACCOUNT_INFO, scopes=SCOPES)
-client = gspread.authorize(credentials)
+# Conexión a Google Sheets sin autenticación (la hoja debe ser pública)
+client = gspread.service_account(filename=None)  # No se usa credenciales.json
 sheet = client.open_by_key(SPREADSHEET_ID).worksheet(SHEET_NAME)
 
 # --- INTERFAZ ---
